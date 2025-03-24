24384d3264023e4d15badd170483c528e095415f
Pranali Sawant <pranalisawant1403@gmail.com>
2025-03-24 22:31:11 +0530

    Create Odd or Even Streetlight System.py

---
+++ Odd or Even Streetlight System.py
@@ -0,0 +1,8 @@
+#Q2. Odd or Even Streetlight System
+
+streetlight_no=int(input("Enter streetlight number:"))
+
+if(streetlight_no%2==0):
+  print("Streetlight is on.")
+else:
+  print("Streetlight is off")
