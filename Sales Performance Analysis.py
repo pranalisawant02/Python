0659b83433e558253d3ce20b5205316822fd669f
Pranali Sawant <pranalisawant1403@gmail.com>
2025-03-31 09:54:37 +0530

    Create Sales Performance Analysis.py

---
+++ Sales Performance Analysis.py
@@ -0,0 +1,10 @@
+#Q1. Sales Performance Analysis
+
+monthly_sales=[5000, 7000, 8000, 6500, 7200,
+9000, 8500]
+
+highest_sales=max(monthly_sales)
+lowest_sales=min(monthly_sales)
+
+print("Highest Sales:", highest_sales)
+print("Lowest Sales:", lowest_sales)
